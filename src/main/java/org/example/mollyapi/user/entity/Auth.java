10989fa24567558c995a3bbb9cba06021f9ca039
LeeJeYoun <dlwpdbs1229@naver.com>
2025-02-07 16:04:55 +0900

    [#10]feat: 회원가입기능 구현 Auth, AuthError 추가

---
+++ src/main/java/org/example/mollyapi/user/entity/Auth.java
@@ -0,0 +1,42 @@
+package org.example.mollyapi.user.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.mollyapi.common.entity.Base;
+import org.example.mollyapi.user.type.Role;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "auth")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SecondaryTable(
+        name = "password",
+        pkJoinColumns = @PrimaryKeyJoinColumn( name = "auth_id") )
+public class Auth extends Base {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long authId;
+
+    private String email;
+
+    private LocalDateTime lastLoginAt;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "FK_AUTH_USER"))
+    private User user;
+
+    @Embedded
+    private Password password;
+
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<Role> role;
+
+}
