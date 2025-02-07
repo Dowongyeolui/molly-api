20377070b6a63874ca51e0a4d1ab8476181aaf40
LeeJeYoun <dlwpdbs1229@naver.com>
2025-02-07 16:02:49 +0900

    [#10]feat: 비밀번호 암호화 메소드 구현 및 솔트 해시 생성 구현

---
+++ src/main/java/org/example/mollyapi/user/entity/Password.java
@@ -0,0 +1,26 @@
+package org.example.mollyapi.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+
+@Getter
+@Embeddable
+@NoArgsConstructor( access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Password {
+
+    @Column(table = "password")
+    private String password;
+
+    @Column(columnDefinition = "BINARY(32)", table = "password")
+    private byte[] salt;
+
+    public static Password of(String password, byte[] salt) {
+        return new Password(password, salt);
+    }
+}
