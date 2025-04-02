4140d9c48dece773a6d5694c0b6e15120913eee0
Jieun714 <jieun98.dev@gmail.com>
2025-04-02 16:56:14 +0900

    [#218] feat: reviewLikeExecutor 추가

--- src/main/java/org/example/mollyapi/common/config/AsyncConfig.java
+++ src/main/java/org/example/mollyapi/common/config/AsyncConfig.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @EnableAsync
@@ -42,6 +43,18 @@ public class AsyncConfig {
         executor.setQueueCapacity(150);
         executor.setThreadNamePrefix("paymentExecutor Async-");
         // 설정 적용
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean(name = "reviewLikeExecutor")
+    public Executor reviewLikeExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);    // 기본 스레드 개수
+        executor.setMaxPoolSize(50);     // 최대 스레드 개수
+        executor.setQueueCapacity(100);  // 대기열 크기
+        executor.setThreadNamePrefix("ReviewLike Async-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
         return executor;
     }
